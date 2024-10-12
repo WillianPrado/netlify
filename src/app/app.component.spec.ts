16324dbb54521224377aa39af4cb339cd3b74409
WillianPrado <willian95prado@gmail.com>
2024-10-12 13:07:02 -0300

    teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'netlify'`, () => {
+  it(`should have as title 'angular_teste'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('netlify');
+    expect(app.title).toEqual('angular_teste');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('netlify app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_teste app is running!');
   });
 });
