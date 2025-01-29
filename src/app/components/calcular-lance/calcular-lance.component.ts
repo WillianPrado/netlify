d8ec7eb3d36323947a9a420ca80c29e8733121d1
WillianPrado <willian95prado@gmail.com>
2025-01-29 20:57:27 -0300

    melhorando o estilo

--- src/app/components/calcular-lance/calcular-lance.component.ts
+++ src/app/components/calcular-lance/calcular-lance.component.ts
@@ -6,15 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./calcular-lance.component.css']
 })
 export class CalcularLanceComponent  {
-  valorFinal: number = 0;
   valorOriginal: number | null = null;
+  valorFinal: number | null = null;
+  valorFinalFormatado: string = '';
 
   calcularValorOriginal() {
-    if (this.valorFinal > 0) {
+    if (this.valorFinal && this.valorFinal > 0) {
       const fator = 1 - 0.30;
       this.valorOriginal = this.valorFinal / fator;
     } else {
       this.valorOriginal = null;
     }
+  }
+
+  onValorFinalChange(valor: string) {
+    // Remove separadores e mantém apenas números
+    let numeroLimpo = valor.replace(/\D/g, '');
+
+    // Atualiza `valorFinal` com o número sem formatação
+    this.valorFinal = numeroLimpo ? parseInt(numeroLimpo, 10) : null;
+
+    // Atualiza a exibição do campo com separador de milhar
+    this.valorFinalFormatado = this.valorFinal ? this.valorFinal.toLocaleString('pt-BR') : '';
+
+    // Calcula o valor original sempre que houver alteração no input
+    this.calcularValorOriginal();
   }
 }
